03f34df36b9ee5097226221727455c48a8ea20f9
GreatTeacher-81 <Mr.M.Thomas2001@gmail.com>
2025-03-29 17:05:38 +0300

    Update token-enabler.sh

--- token-enabler.sh
+++ token-enabler.sh
@@ -7,6 +7,6 @@ if [ -z "$TOKEN_LICHESS" ]; then
   echo "Error: Environment variable TOKEN_LICHESS is not set."
   exit 1
 else
-  # This Code Below is Gonna Replace the placeholder text on the first line of config.yaml to apply the token! 
-  sed -i.bak "1s#Automatically-Applyed-Here-if-U-Have-Been-Type-it-in-Environment-Variables-TOKEN_LICHESS#$TOKEN_LICHESS#" config.yaml
+  # This Code Below is Gonna Replace the placeholder text on the first line of config.yml to apply the token! 
+  sed -i.bak "1s#Automatically-Applyed-Here-if-U-Have-Been-Type-it-in-Environment-Variables-TOKEN_LICHESS#$TOKEN_LICHESS#" config.yml
 fi
